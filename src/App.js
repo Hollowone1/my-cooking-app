65616f339e6d1d173371d6cf0efba30e6675f1a0
Antoine Stoecklin <astoeck@hotmail.fr>
2023-05-11 17:20:07 +0200

    tailwind cards done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
